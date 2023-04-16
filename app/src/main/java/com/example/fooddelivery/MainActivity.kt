792a34c9d96609b0ac0972048d7a7bd1257e0ffa
UlugbekYarkinov <yarkinovulugbek@gmail.com>
2023-04-17 00:31:38 +0500

    menu button composable added

--- app/src/main/java/com/example/fooddelivery/MainActivity.kt
+++ app/src/main/java/com/example/fooddelivery/MainActivity.kt
@@ -3,38 +3,68 @@ package com.example.fooddelivery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.fooddelivery.ui.theme.CardItemBg
 import com.example.fooddelivery.ui.theme.FoodDeliveryTheme
+import com.example.fooddelivery.ui.theme.IconColor
+import com.example.fooddelivery.ui.theme.Yellow500
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FoodDeliveryTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun BoxWithRes(
+    resId: Int,
+    description: String,
+    bgColor: Color? = CardItemBg,
+    iconColor: Color? = IconColor,
+    boxSize: Int? = 40,
+    iconSize: Int = 24) {
+        Box(
+            modifier = Modifier
+                .size(boxSize!!.dp)
+                .clip(RoundedCornerShape(10.dp))
+                .background(bgColor!!),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painter = painterResource(id = resId),
+                contentDescription = description,
+                modifier = Modifier
+                    .size(iconSize.dp),
+                tint = iconColor!!
+            )
+        }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     FoodDeliveryTheme {
-        Greeting("Android")
+        BoxWithRes(resId = R.drawable.menu, bgColor = Yellow500, description = "Menu")
     }
 }
