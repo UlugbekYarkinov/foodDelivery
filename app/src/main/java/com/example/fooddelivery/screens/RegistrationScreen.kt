51383fc7c2b1f8329bb614e6f90b76117e59ad82
UlugbekYarkinov <yarkinovulugbek@gmail.com>
2023-04-28 18:59:38 +0500

    design changed

--- app/src/main/java/com/example/fooddelivery/screens/RegistrationScreen.kt
+++ app/src/main/java/com/example/fooddelivery/screens/RegistrationScreen.kt
@@ -1,14 +1,24 @@
 package com.example.fooddelivery.screens
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.fooddelivery.Destinations
+import com.example.fooddelivery.ui.theme.Yellow500
 import com.google.firebase.auth.FirebaseAuth
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 
 fun RegistrationScreen(navController: NavController) {
@@ -16,38 +26,95 @@ fun RegistrationScreen(navController: NavController) {
     var password by remember { mutableStateOf("") }
     var error by remember { mutableStateOf("") }
 
-    Column(modifier = Modifier.fillMaxWidth()) {
-        OutlinedTextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text("Email") },
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Password") },
-            modifier = Modifier.fillMaxWidth()
-        )
-        Button(
-            onClick = {
-                FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener { task ->
-                        if (task.isSuccessful) {
-                            navController.navigate(Destinations.Home)
-                        } else {
-                            error = task.exception?.localizedMessage ?: "Unknown error"
-                        }
-                    }
-            },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Register")
-        }
-        if (error.isNotEmpty()) {
-            Text(error, color = MaterialTheme.colorScheme.error)
-        }
-    }
+    Scaffold(
+
+        topBar = {
+            // App bar content
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "FOOD DELIVERY",
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 24.sp,
+                        color = Yellow500
+                    )
+                )
+            }
+        },
+        content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(horizontal = 20.dp),
+                verticalArrangement = Arrangement.Center) {
+                OutlinedTextField(
+                    value = email,
+                    onValueChange = { email = it },
+                    label = { Text("Email") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                OutlinedTextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    label = { Text("Password") },
+                    modifier = Modifier.fillMaxWidth(),
+                    visualTransformation = PasswordVisualTransformation()
+                )
+                Spacer(modifier = Modifier.height(30.dp))
+                Button(
+                    onClick = {
+                        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener { task ->
+                                if (task.isSuccessful) {
+                                    navController.navigate(Destinations.Home)
+                                } else {
+                                    error = task.exception?.localizedMessage ?: "Unknown error"
+                                }
+                            }
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Yellow500
+                    )
+                ) {
+                    Text("Register", style = TextStyle(fontSize = 20.sp))
+                }
+                if (error.isNotEmpty()) {
+                    Text(
+                        error,
+                        color = MaterialTheme.colorScheme.error,
+                        style = TextStyle(fontSize = 14.sp)
+                    )
+                } else {
+                    Text(text = "")
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "Back to ",
+                        style = TextStyle(fontSize = 14.sp)
+                    )
+                    Text(
+                        text = "Login",
+                        modifier = Modifier.clickable {
+                            navController.popBackStack()
+                        },
+                        style = TextStyle(
+                            color = Color.Blue,
+                            fontSize = 14.sp
+                        )
+                    )
+                }
+            }
+        },
+    )
 
 }
